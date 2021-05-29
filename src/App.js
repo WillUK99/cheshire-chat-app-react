fd72ab21ee1d0143db60342e9ef9649e6a6097ab
WillUK99 <willuk1999@gmail.com>
2021-05-29 12:31:19 +0100

    set up basic file structure | added basic comments as to where things should be going and also installed all needed packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+// import { AuthProvider } from "../contexts/AuthContext"
+
+// import Chats from "./Chats"
+// import Login from "./Login"
+
+const App = () => {
+    console.log("welcome to cheshire chat")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        {/* <AuthProvider> */}
+          <Switch>
+            {/* <Route path="/chats" component={Chats} />
+            <Route path="/" component={Login} /> */}
+          </Switch>
+        {/* </AuthProvider> */}
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
