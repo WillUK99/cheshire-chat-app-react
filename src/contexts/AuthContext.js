989faec819a821e7349de5f6cb71b29b60bc1660
WillUK99 <willuk1999@gmail.com>
2021-05-29 17:09:05 +0100

    added AuthContext and getting user data from firebase auth to then redirect to /chats page

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,35 @@
 import React, { useContext, useState, useEffect } from "react"
 import { useHistory } from "react-router-dom"
+import { auth } from "../firebase"
 
+// creating context
 const AuthContext = React.createContext()
 
+// creating function w=to use later to grab context
 export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider = ({ children }) => {
+    // setting user and loader states
+    const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState({})
+    const history = useHistory()
+
+    useEffect(() => {
+        // getting user from firebase auth
+        auth.onAuthStateChanged((user) => {
+            // setting user from firebase auth to state
+            setUser(user)
+            setLoading(false)
+            //using react-router-dom to redirect to new page
+            history.push("/chats")
+        })
+    }, [user, history]);
+
+    const value = { user }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
